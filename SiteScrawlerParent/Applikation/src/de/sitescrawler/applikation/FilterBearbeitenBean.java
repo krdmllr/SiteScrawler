9d635877ca6396662f3df37d2c7c4076c5a9b802
KrdMlr <k.l.mueller@outlook.com>
2017-03-30 21:52:04 +0200

    Beans und fehlende PrimeFace dependencys hinzugefuegt.

---
+++ SiteScrawlerParent/Applikation/src/de/sitescrawler/applikation/FilterBearbeitenBean.java
@@ -0,0 +1,13 @@
+package de.sitescrawler.applikation;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@SessionScoped
+@Named("filterbearbeiten")
+public class FilterBearbeitenBean implements Serializable
+{
+	private static final long serialVersionUID = 1L;
+}
