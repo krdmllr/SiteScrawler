96194f6cba7d97b4ef793acf74c06077f531181b
KrdMlr <k.l.mueller@outlook.com>
2017-05-06 16:13:34 +0200

    Config.properties kann auch aus projekt root geladen werden.

--- SiteScrawlerParent/Model/src/de/sitescrawler/model/ProjectConfig.java
+++ SiteScrawlerParent/Model/src/de/sitescrawler/model/ProjectConfig.java
@@ -1,5 +1,6 @@
 package de.sitescrawler.model;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.nio.file.Path;
@@ -24,8 +25,15 @@ public class ProjectConfig {
 
 		if (configPath == null || configPath.isEmpty()) {
 			//Perhabs server is not running
-			System.out.println("Could not resolve config path.");
-			configPath = "C:/Users/klmue/Source/server/standalone/configuration";
+			System.out.println("Could not resolve config path. Searching in project path for config instead.");
+			String currentDir = System.getProperty("user.dir");
+			File currentFile = new File(currentDir);
+			
+			while(!currentFile.getName().toLowerCase().equals("sitescrawler")){
+				currentFile = currentFile.getParentFile();
+			}
+			configPath =  currentFile.getAbsolutePath();
+			System.out.println("Present Project Directory : " + configPath);
 		}
 
 		String configName = "config.properties";
