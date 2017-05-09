6d3d17c31ad4fbcad2a0b71f36f39765446bbdd0
robin <robin.schmidt.97@web.de>
2017-05-09 20:31:02 +0200

    formatting

--- SiteScrawlerParent/Crawler/src/de/sitescrawler/services/crawler/Verarbeitung.java
+++ SiteScrawlerParent/Crawler/src/de/sitescrawler/services/crawler/Verarbeitung.java
@@ -35,147 +35,142 @@ import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 /**
- * @author Tobias, Yvette Verarbeitet eine Quelle. Durchsucht den RSS Feed der Quelle, parst die Artikel und gibt die
- *         gefundenen Artikel an Solr weiter.
+ * @author Tobias, Yvette Verarbeitet eine Quelle. Durchsucht den RSS Feed der
+ *         Quelle, parst die Artikel und gibt die gefundenen Artikel an Solr
+ *         weiter.
  *
  */
 @RequestScoped
 @Named
-class Verarbeitung
-{
-    // Globalen Logger holen
-    private final static Logger           LOGGER = Logger.getLogger("de.sitescrawler.logger");
+class Verarbeitung {
+	// Globalen Logger holen
+	private final static Logger LOGGER = Logger.getLogger("de.sitescrawler.logger");
 
-    @Inject
-    private ISolrService                  solrService;
-    @Inject
-    private IFiltergruppenZugriffsManager filtergruppenZugriffsManager;
+	@Inject
+	private ISolrService solrService;
+	@Inject
+	private IFiltergruppenZugriffsManager filtergruppenZugriffsManager;
 
-    public Verarbeitung()
-    {
-    }
+	public Verarbeitung() {
+	}
 
-    /**
-     * Startet und führt das Zuschneiden der Artikel aus den Quellen aus.
-     */
-    public List<Artikel> durchsucheQuelle(boolean sendeAnSolr, Quelle quelle)
-    {
+	/**
+	 * Startet und führt das Zuschneiden der Artikel aus den Quellen aus.
+	 */
+	public List<Artikel> durchsucheQuelle(boolean sendeAnSolr, Quelle quelle) {
 
-        List<Artikel> gefundeneArtikel = new ArrayList<>();
+		List<Artikel> gefundeneArtikel = new ArrayList<>();
 
-        Verarbeitung.LOGGER.log(Level.INFO, quelle.toString() + "...");
+		Verarbeitung.LOGGER.log(Level.INFO, quelle.toString() + "...");
 
-        ArtikelZurechtschneiden artikelZurechtschneiden = new ArtikelZurechtschneiden();
+		ArtikelZurechtschneiden artikelZurechtschneiden = new ArtikelZurechtschneiden();
 
-        try
-        {
-            // Parse RSS Feed
-            SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(quelle.getRsslink())));
+		try {
+			// Parse RSS Feed
+			SyndFeed feed = new SyndFeedInput().build(new XmlReader(new URL(quelle.getRsslink())));
 
-            // Gehe jeden Eintrag des RSS Feeds durch und crawle die hinterlegte Website nach dem Volltext
-            List<SyndEntry> entries = feed.getEntries();
-            for (SyndEntry entry : entries)
-            {
-                Verarbeitung.LOGGER.log(Level.INFO, "Parse: " + entry.getUri());
+			// Gehe jeden Eintrag des RSS Feeds durch und crawle die hinterlegte
+			// Website nach dem Volltext
+			List<SyndEntry> entries = feed.getEntries();
+			for (SyndEntry entry : entries) {
+				Verarbeitung.LOGGER.log(Level.INFO, "Parse: " + entry.getUri());
 
-                // Speichere wichtige Eigenschaften zwischen
-                String autor = entry.getAuthor();
-                String beschreibung = entry.getDescription().getValue();
-                String regex = "<a.*/a>";
-                beschreibung = beschreibung.replaceAll(regex, "");
-                String link = entry.getLink();
-                Date veroeffentlichungsDatum = entry.getPublishedDate();
-                String titel = entry.getTitle();
+				// Speichere wichtige Eigenschaften zwischen
+				String autor = entry.getAuthor();
+				String beschreibung = entry.getDescription().getValue();
+				String regex = "<a.*/a>";
+				beschreibung = beschreibung.replaceAll(regex, "");
+				String link = entry.getLink();
+				Date veroeffentlichungsDatum = entry.getPublishedDate();
+				String titel = entry.getTitle();
 
-                // Crawle die Website des Artikels nach dem Text ab
-                List<String> absaetze = new ArrayList<>();
-                try
-                {
-                    absaetze = artikelZurechtschneiden.getAbsaetze(link, quelle);
-                }
-                catch (UnparsbarException e1)
-                {
-                    Verarbeitung.LOGGER.log(Level.SEVERE, "Fehler beim Parsen der Absätze: " + entry.getUri());
-                    // TODO Exceptions besser aufschlüsseln
-                    e1.printStackTrace();
-                }
+				// Crawle die Website des Artikels nach dem Text ab
+				List<String> absaetze = new ArrayList<>();
+				try {
+					absaetze = artikelZurechtschneiden.getAbsaetze(link, quelle);
+				} catch (UnparsbarException e1) {
+					Verarbeitung.LOGGER.log(Level.SEVERE, "Fehler beim Parsen der Absätze: " + entry.getUri());
+					// TODO Exceptions besser aufschlüsseln
+					e1.printStackTrace();
+				}
 
-                Verarbeitung.LOGGER.log(Level.INFO, String.format("Added Titel: %s%n Autor: %s%n Link: %s%n Datum: %s%n Beschreibung:%s%n Absätze:%s%n", titel,
-                                autor, link, veroeffentlichungsDatum, beschreibung, absaetze.toString()));
+				Verarbeitung.LOGGER.log(Level.INFO,
+						String.format(
+								"Added Titel: %s%n Autor: %s%n Link: %s%n Datum: %s%n Beschreibung:%s%n Absätze:%s%n",
+								titel, autor, link, veroeffentlichungsDatum, beschreibung, absaetze.toString()));
 
-                // Erstelle einen neuen Artikel aus den gefundenen Daten und übergebe ihn an Solr
-                Artikel artikel = new Artikel(veroeffentlichungsDatum, autor, titel, beschreibung, link, absaetze);
+				// Erstelle einen neuen Artikel aus den gefundenen Daten und
+				// übergebe ihn an Solr
+				Artikel artikel = new Artikel(veroeffentlichungsDatum, autor, titel, beschreibung, link, absaetze);
 
-                gefundeneArtikel.add(artikel);
+				gefundeneArtikel.add(artikel);
 
-            }
-            if (sendeAnSolr)
-            {
-                this.solrService.addArtikel(gefundeneArtikel);
-            }
-        }
-        catch (IllegalArgumentException | FeedException | IOException e)
-        {
-            Verarbeitung.LOGGER.log(Level.SEVERE, "Fehler beim Parsen der Seite!");
-            e.printStackTrace();
-        }
+			}
+			if (sendeAnSolr) {
+				this.solrService.addArtikel(gefundeneArtikel);
+			}
+		} catch (IllegalArgumentException | FeedException | IOException e) {
+			Verarbeitung.LOGGER.log(Level.SEVERE, "Fehler beim Parsen der Seite!");
+			e.printStackTrace();
+		}
 
-        Verarbeitung.LOGGER.log(Level.INFO, "Crawl von " + quelle.getName() + " fertig. " + gefundeneArtikel.size() + " Artikel gefunden.");
-        return gefundeneArtikel;
-    }
+		Verarbeitung.LOGGER.log(Level.INFO,
+				"Crawl von " + quelle.getName() + " fertig. " + gefundeneArtikel.size() + " Artikel gefunden.");
+		return gefundeneArtikel;
+	}
 
-    public List<Artikel> durchsucheTwitter(boolean sendeAnSolr)
-    {
-        List<Artikel> gefundeneArtikel = new ArrayList<>();
-        
-        ProjectConfig projectConfig = new ProjectConfig();
-        
-        String consumerKey = projectConfig.getConsumerKey();
-        String consumerSecret = projectConfig.getConsumerSecret();
-        String accessToken = projectConfig.getAccessToken();
-        String accessTokenSecret = projectConfig.getAccessTokenSecret();
-        
-        //Authentication
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-              .setOAuthConsumerKey(consumerKey)
-              .setOAuthConsumerSecret(consumerSecret)
-              .setOAuthAccessToken(accessToken)
-              .setOAuthAccessTokenSecret(accessTokenSecret);
-        
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
-        
-        try {
-        	//Trends für Deutschland mittels WOEID holen
-			Trends trends = twitter.getPlaceTrends(23424829); 
-			
+	public List<Artikel> durchsucheTwitter(boolean sendeAnSolr) {
+		List<Artikel> gefundeneArtikel = new ArrayList<>();
+
+		ProjectConfig projectConfig = new ProjectConfig();
+
+		String consumerKey = projectConfig.getConsumerKey();
+		String consumerSecret = projectConfig.getConsumerSecret();
+		String accessToken = projectConfig.getAccessToken();
+		String accessTokenSecret = projectConfig.getAccessTokenSecret();
+
+		// Authentication
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true).setOAuthConsumerKey(consumerKey).setOAuthConsumerSecret(consumerSecret)
+				.setOAuthAccessToken(accessToken).setOAuthAccessTokenSecret(accessTokenSecret);
+
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+
+		try {
+			// Trends für Deutschland mittels WOEID holen
+			Trends trends = twitter.getPlaceTrends(23424829);
+
 			// Für jeden Trend neue Query erzeugen und die Tweets lesen
 			for (int i = 0; i < trends.getTrends().length; i++) {
-	            String suchstring = trends.getTrends()[i].getQuery();
-	            Query query = new Query(suchstring);
-	            QueryResult result;
-	            result = twitter.search(query);
-	            List<Status> tweets = result.getTweets();
-	            
-	            //Artikel aus den Tweets erzeugen
-	            for (Status tweet : tweets) {
-	            	Artikel artikel = new Artikel(tweet.getCreatedAt(),tweet.getUser().getScreenName(),"Tweet" + tweet.getId(), tweet.getText(),tweet.getSource()); //überprüfen ob richtig erstellt
-	            	gefundeneArtikel.add(artikel);
-	            	Verarbeitung.LOGGER.log(Level.INFO, "Tweet gefunden: @" + tweet.getUser().getScreenName() + " - " + tweet.getText());
-	            }
-	        }
-			if (sendeAnSolr)
-            {
-                this.solrService.addArtikel(gefundeneArtikel);
-            }
+				String suchstring = trends.getTrends()[i].getQuery();
+				Query query = new Query(suchstring);
+				QueryResult result;
+				result = twitter.search(query);
+				List<Status> tweets = result.getTweets();
+
+				// Artikel aus den Tweets erzeugen
+				for (Status tweet : tweets) {
+
+					// TODO: überprüfen ob Artikel richtig erstellt
+					Artikel artikel = new Artikel(tweet.getCreatedAt(), tweet.getUser().getScreenName(),
+							"Tweet" + tweet.getId(), tweet.getText(), tweet.getSource());
+					gefundeneArtikel.add(artikel);
+					Verarbeitung.LOGGER.log(Level.INFO,
+							"Tweet gefunden: @" + tweet.getUser().getScreenName() + " - " + tweet.getText());
+				}
+			}
+			if (sendeAnSolr) {
+				this.solrService.addArtikel(gefundeneArtikel);
+			}
 		} catch (TwitterException e) {
 			Verarbeitung.LOGGER.log(Level.SEVERE, "Fehler beim holen der Trends!");
 			e.printStackTrace();
-		} 
-        
-        Verarbeitung.LOGGER.log(Level.INFO, "Crawl von Twitter abgeschlossen. Tweets gefunden: " + gefundeneArtikel.size() );
-        return gefundeneArtikel;
-    }
+		}
+
+		Verarbeitung.LOGGER.log(Level.INFO,
+				"Crawl von Twitter abgeschlossen. Tweets gefunden: " + gefundeneArtikel.size());
+		return gefundeneArtikel;
+	}
 
 }
