9043aa835724c83f35f913a5a49eec36c1b378ce
Zenty <yvette.labastille@gmail.com>
2017-05-14 18:06:53 +0200

    Unabhängig von Xml-Datei, Kommentare
    Formatierung sollte jetzt keine Datei mehr erstellen, sondern mit
    Streams arbeiten. Testen kommt noch.
    Methoden kommentiert.

--- SiteScrawlerParent/Formatierung/src/de/sitescrawler/formatierung/FormatiererService.java
+++ SiteScrawlerParent/Formatierung/src/de/sitescrawler/formatierung/FormatiererService.java
@@ -2,7 +2,6 @@ package de.sitescrawler.formatierung;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.text.DateFormat;
@@ -77,14 +76,13 @@ public class FormatiererService implements IFormatiererService
      *
      * @param archiveintrag
      *            Der Archiveintrag, der umgewandelt werden soll.
-     * @return Umgewandelter Archiveintrag als HTML-Format.
+     * @return Umgewandelter Archiveintrag in HTML-Format als String.
      */
     @Override
     public String generiereHtmlZusammenfassung(Archiveintrag archiveintrag)
     {
         String htmlAusgabe = "";
         Set<Artikel> artikelAusArchiveintrag = archiveintrag.getArtikel();
-        HTMLHelfer htmlHelfer = new HTMLHelfer();
 
         // Wandelt alle Artikel aus dem Archiveintrag in eine HTML-Datei um
         if (!artikelAusArchiveintrag.isEmpty())
@@ -104,7 +102,7 @@ public class FormatiererService implements IFormatiererService
     }
 
     /**
-     * Wandelt den Archiveintrag in eine PDF-Datei um.
+     * Wandelt den Archiveintrag in eine PDF-Datei als ByteArrayDataSource um.
      *
      * @param archiveintrag
      *            Der Archiveintrag, der umgewandelt werden soll.
@@ -113,33 +111,7 @@ public class FormatiererService implements IFormatiererService
     @Override
     public ByteArrayDataSource generierePdfZusammenfassung(Archiveintrag archiveintrag)
     {
-        // TODO in eine Methode
-        File xmlDatei = new File("artikelXML.xml");
-        String aktuellesDatum = this.wandleDatumUm(new Date());
-        ByteArrayOutputStream pdfOut = new ByteArrayOutputStream();
-
-        Map<String, String> parameterFuerPDF = this.erstelleParameter(aktuellesDatum);
-
-        try
-        {
-            this.wandelArchiveintragInXML(archiveintrag, xmlDatei);
-            // Falls Archiveintrag leer ist: XML nur mit <archiveintrag/> -> PDF-Generierung geht
-            FormatiererService.LOGGER.log(Level.INFO, "Umwandlung des Archiveintrags in XML erfolgreich.");
-        }
-        catch (JAXBException e2)
-        {
-            FormatiererService.LOGGER.log(Level.SEVERE, "Fehler bei Umwandlung des Archiveintrags in XML.", e2);
-        }
-
-        try
-        {
-            pdfOut = this.wandleXMLinPDF(xmlDatei, pdfOut, parameterFuerPDF);
-            FormatiererService.LOGGER.log(Level.INFO, "Umwandlung des Archiveintrags von XML in PDF erfolgreich.");
-        }
-        catch (IOException e1)
-        {
-            FormatiererService.LOGGER.log(Level.SEVERE, "Fehler bei Umwandlung des Archiveintrags von XML in PDF.", e1);
-        }
+        ByteArrayOutputStream pdfOut = generierePDFAlsByteArrayOutputStream(archiveintrag);
         ByteArrayInputStream pdfIn = new ByteArrayInputStream(pdfOut.toByteArray());
         ByteArrayDataSource daten = null;
 
@@ -157,6 +129,78 @@ public class FormatiererService implements IFormatiererService
         return daten;
     }
 
+    /**
+     * Wandelt den Archiveintrag in eine PDF-Datei als ByteArrayOutputStream um.
+     * 
+     * @param archiveintrag
+     * @return Umgewandelter Archiveintrag als ByteArrayOutputStream.
+     */
+    private ByteArrayOutputStream generierePDFAlsByteArrayOutputStream(Archiveintrag archiveintrag)
+    {
+        ByteArrayOutputStream xmlOutputStream = new ByteArrayOutputStream();
+        ByteArrayOutputStream pdfOut = new ByteArrayOutputStream();
+        String aktuellesDatum = this.wandleDatumUm(new Date());
+
+        Map<String, String> parameterFuerPDF = this.erstelleParameter(aktuellesDatum);
+
+        try
+        {
+            // Falls Archiveintrag leer ist: XML nur mit <archiveintrag/> -> PDF-Generierung geht
+            xmlOutputStream = this.pdfHelfer.schreibeArtikelAlsXML(archiveintrag);
+            FormatiererService.LOGGER.log(Level.INFO, "Umwandlung des Archiveintrags in XML erfolgreich.");
+        }
+        catch (JAXBException e2)
+        {
+            FormatiererService.LOGGER.log(Level.SEVERE, "Fehler bei Umwandlung des Archiveintrags in XML.", e2);
+        }
+
+        try
+        {
+            pdfOut = this.wandleXMLinPDF(xmlOutputStream, pdfOut, parameterFuerPDF);
+            FormatiererService.LOGGER.log(Level.INFO, "Umwandlung des Archiveintrags von XML in PDF erfolgreich.");
+        }
+        catch (IOException e1)
+        {
+            FormatiererService.LOGGER.log(Level.SEVERE, "Fehler bei Umwandlung des Archiveintrags von XML in PDF.", e1);
+        }
+        return pdfOut;
+    }
+
+    /**
+     * Wandelt den Archiveintrag in eine PDF-Datei als ByteArrayOutputStream um.
+     *
+     * @param archiveintrag
+     *            Der Archiveintrag, der umgewandelt werden soll.
+     * @return Umgewandelter Archiveintrag als ByteArrayOutputStream.
+     */
+    @Override
+    public ByteArrayOutputStream generierePdfZusammenfassungStream(Archiveintrag archiveintrag)
+    {
+        return generierePDFAlsByteArrayOutputStream(archiveintrag);
+    }
+
+    /**
+     * Wandelt das übergebene Datum in eine leserliche Form um.
+     *
+     * @param datumUmzuwandeln
+     * @return
+     */
+    public String wandleDatumUm(Date datumUmzuwandeln)
+    {
+        DateFormat df;
+
+        df = DateFormat.getDateInstance(DateFormat.LONG, Locale.GERMAN);
+        String aktuellesDatum = df.format(datumUmzuwandeln);
+
+        return aktuellesDatum;
+    }
+
+    /**
+     * Setzt die Parameter für die Umwandlung von Archiveintrag in PDF.
+     * 
+     * @param aktuellesDatum
+     * @return
+     */
     private Map<String, String> erstelleParameter(String aktuellesDatum)
     {
         Map<String, String> parameterFuerPDF = new HashMap<String, String>()
@@ -186,7 +230,8 @@ public class FormatiererService implements IFormatiererService
      * @return
      * @throws IOException
      */
-    private ByteArrayOutputStream wandleXMLinPDF(File xmlDatei, ByteArrayOutputStream pdf, Map<String, String> parameterFuerPDF) throws IOException
+    private ByteArrayOutputStream wandleXMLinPDF(ByteArrayOutputStream xmlDatei, ByteArrayOutputStream pdf, Map<String, String> parameterFuerPDF)
+        throws IOException
     {
         try
         {
@@ -204,67 +249,4 @@ public class FormatiererService implements IFormatiererService
         return pdf;
     }
 
-    /**
-     * Archiveintrag in XML-Datei umwandeln.
-     *
-     * @param archiveintrag
-     * @param pdfHelfer
-     * @param xmlDatei
-     * @throws JAXBException
-     */
-    private void wandelArchiveintragInXML(Archiveintrag archiveintrag, File xmlDatei) throws JAXBException
-    {
-        this.pdfHelfer.schreibeArtikelAlsXML(archiveintrag, xmlDatei);
-    }
-
-    /**
-     * Wandelt das übergebene Datum in eine leserliche Form um.
-     *
-     * @param datumUmzuwandeln
-     * @return
-     */
-    public String wandleDatumUm(Date datumUmzuwandeln)
-    {
-        DateFormat df;
-
-        df = DateFormat.getDateInstance(DateFormat.LONG, Locale.GERMAN);
-        String aktuellesDatum = df.format(datumUmzuwandeln);
-
-        return aktuellesDatum;
-    }
-
-    @Override
-    public ByteArrayOutputStream generierePdfZusammenfassungStream(Archiveintrag archiveintrag)
-    {
-        // TODO mit streams
-        // TODO in eine Methode
-
-        File xmlDatei = new File("artikelXML.xml");
-        String aktuellesDatum = this.wandleDatumUm(new Date());
-        ByteArrayOutputStream pdfOut = new ByteArrayOutputStream();
-
-        Map<String, String> parameterFuerPDF = this.erstelleParameter(aktuellesDatum);
-
-        try
-        {
-            this.wandelArchiveintragInXML(archiveintrag, xmlDatei);
-            FormatiererService.LOGGER.log(Level.INFO, "Umwandlung des Archiveintrags in XML erfolgreich.");
-        }
-        catch (JAXBException e2)
-        {
-            FormatiererService.LOGGER.log(Level.SEVERE, "Fehler bei Umwandlung des Archiveintrags in XML.", e2);
-        }
-
-        try
-        {
-            pdfOut = this.wandleXMLinPDF(xmlDatei, pdfOut, parameterFuerPDF);
-            FormatiererService.LOGGER.log(Level.INFO, "Umwandlung des Archiveintrags von XML in PDF erfolgreich.");
-            return pdfOut;
-        }
-        catch (IOException e1)
-        {
-            FormatiererService.LOGGER.log(Level.SEVERE, "Fehler bei des Archiveintrags");
-        }
-        return null;
-    }
 }
