fb6c60867978a5f6dba188fdddcb04059c826eaa
Aldaron7 <marcel4hbstar@gmx.de>
2017-05-16 23:45:25 +0200

    scheduler um report methode ergaenzt

--- SiteScrawlerParent/Scheduler/src/de/sitescrawler/scheduler/Scheduler.java
+++ SiteScrawlerParent/Scheduler/src/de/sitescrawler/scheduler/Scheduler.java
@@ -1,12 +1,15 @@
 package de.sitescrawler.scheduler;
 
+import java.time.LocalDateTime;
 import java.util.logging.Logger;
 
+import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
 
 import de.sitescrawler.crawler.interfaces.ICrawlerLaufService;
 import de.sitescrawler.jpa.management.interfaces.IQuellenManager;
+import de.sitescrawler.report.IReportService;
 
 @Singleton
 public class Scheduler
@@ -19,11 +22,22 @@ public class Scheduler
     @Inject
     private IQuellenManager     quellenManager;
 
-    // @Schedule(second = "0", minute = "*/30", hour = "*", persistent = false)
+    @Inject
+    private IReportService      reportService;
+
+    @Schedule(second = "0", minute = "*/30", hour = "*", persistent = false)
     public void starteCrawl()
     {
         Scheduler.LOGGER.info("Scheduler starte Crawlvorgang...");
         this.quellenManager.ladeQuellenEin();
         this.crawlerLaufService.crawl();
     }
+
+    @Schedule(second = "0", minute = "50", hour = "*", persistent = false)
+    public void erstelleReports()
+    {
+        Scheduler.LOGGER.info("Scheduler starte Reporting...");
+        this.reportService.generiereReports(LocalDateTime.now());
+    }
+
 }
