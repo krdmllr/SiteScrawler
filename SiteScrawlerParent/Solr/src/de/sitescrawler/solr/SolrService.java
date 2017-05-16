b1076997d4d8b9b69a434d747e84c6b0b83d0a6b
KrdMlr <k.l.mueller@outlook.com>
2017-05-17 00:31:08 +0200

    Arbeite wieder auf richtier Solr Instanz.

--- SiteScrawlerParent/Solr/src/de/sitescrawler/solr/SolrService.java
+++ SiteScrawlerParent/Solr/src/de/sitescrawler/solr/SolrService.java
@@ -43,8 +43,8 @@ public class SolrService implements ISolrService, Serializable
 
     // TODO: in config-Datei auslagern
     // private static final String SolrUrl = "http://sitescrawler.de:8983/solr/testdaten";
-    private static final String SolrUrl = "http://sitescrawler.de:8983/solr/spielwiesewilliam";
-    //private static final String           SolrUrl          = "http://sitescrawler.de:8983/solr/sitescrawler_dev_solr";
+    //private static final String SolrUrl = "http://sitescrawler.de:8983/solr/spielwiesewilliam";
+    private static final String           SolrUrl          = "http://sitescrawler.de:8983/solr/sitescrawler_dev_solr";
     private static final SimpleDateFormat formatter        = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
     public SolrService()
